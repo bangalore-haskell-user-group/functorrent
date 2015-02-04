58bb9d54d0d8b31bef6c855ba4c6048b3cdc3f72
Ramakrishnan Muthukrishnan <ram@rkrishnan.org>
2015-02-04 21:25:06 +0530

    WIP: info record

--- src/Metainfo.hs
+++ src/Metainfo.hs
@@ -1,9 +1,19 @@
 module Metainfo where
 
 import qualified Bencode as Benc
+import qualified Data.ByteString.Char8 as BC
 import Data.Time.Clock
 
-data Metainfo = MetaInfo { info :: Benc.BVal
+-- only single file mode supported for the time being.
+data Info = Info { pieceLength :: Integer
+                 , pieces :: BC.ByteString
+                 , private :: Maybe Integer
+                 , name :: String
+                 , lengthInBytes :: Integer
+                 , md5sum :: Maybe String }
+          deriving (Eq)
+
+data Metainfo = MetaInfo { info :: Info
                          , announce :: String
                          , announceList :: Maybe [[String]]
                          , creationDate :: Maybe UTCTime
