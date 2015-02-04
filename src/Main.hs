98d545d08350a363724b70e69b4311b465a771c9
Ramakrishnan Muthukrishnan <ram@rkrishnan.org>
2015-02-04 21:01:20 +0530

    WIP: create MetaInfo structure from torrentfile

--- src/Main.hs
+++ src/Main.hs
@@ -3,10 +3,17 @@ module Main where
 import System.Environment (getArgs)
 import qualified Data.ByteString.Char8 as BC
 import qualified Bencode as Benc
+import qualified Metainfo as MInfo
+import Text.ParserCombinators.Parsec
+
+printError :: ParseError -> IO ()
+printError e = putStrLn "parse error"
 
 main :: IO ()
 main = do
   args <- getArgs
   torrentStr <- BC.readFile (head args)
-  let metadata = Benc.decode torrentStr
+  case (Benc.decode torrentStr) of
+   Right d -> MInfo.mkMetaInfo d
+   Left e -> printError e
   putStrLn "done"
---
+++ src/Metainfo.hs
@@ -0,0 +1,1 @@
+module Metainfo where
