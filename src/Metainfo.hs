691a14afae70ce22c9752bcdeacdc2fea7316dbf
Ramakrishnan Muthukrishnan <ram@rkrishnan.org>
2015-02-04 21:10:51 +0530

    WIP: metainfo record

--- src/Metainfo.hs
+++ src/Metainfo.hs
@@ -1,1 +1,15 @@
 module Metainfo where
+
+import qualified Bencode as Benc
+import Data.Time.Clock
+
+data Metainfo = MetaInfo { info :: Benc.BVal
+                         , announce :: String
+                         , announceList :: Maybe [[String]]
+                         , creationDate :: Maybe UTCTime
+                         , comment :: Maybe String
+                         , createdBy :: Maybe String
+                         , encoding :: Maybe String }
+              deriving (Eq)
+
+mkMetaInfo :: Benv.BVal -> MetaInfo
