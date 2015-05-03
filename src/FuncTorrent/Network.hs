cfd4492b3eaaa60332ea25af64317d4d39fb511a
Ramakrishnan Muthukrishnan <ram@rkrishnan.org>
2015-05-03 14:44:58 +0530

    Remove the redundant withSocketsDo call wrapping simpleHTTP
    simpleHTTP calls withSocketsDo deep inside, so no need to wrap
    it outside.

--- src/FuncTorrent/Network.hs
+++ src/FuncTorrent/Network.hs
@@ -11,7 +11,6 @@ import Data.ByteString (ByteString)
 import Data.ByteString.Char8 as BC (pack, unpack, concat, intercalate)
 import Network.HTTP (simpleHTTP, defaultGETRequest_, getResponseBody)
 import Network.URI (parseURI)
-import Network.Socket (withSocketsDo)
 
 -- | Make a query string from a alist of k, v
 -- TODO: Url encode each argument
@@ -19,7 +18,7 @@ mkParams :: [(String, ByteString)] -> ByteString
 mkParams params = BC.intercalate "&" [concat [pack f, "=", s] | (f,s) <- params]
 
 get :: String -> [(String, ByteString)] -> IO ByteString
-get url args = withSocketsDo $ simpleHTTP (defaultGETRequest_ url') >>= getResponseBody
+get url args = simpleHTTP (defaultGETRequest_ url') >>= getResponseBody
     where url' = case parseURI $ unpack $ concat [pack url, "?", qstr] of
                    Just x -> x
                    _ -> error "Bad tracker URL"
